a5656a887b706eb900e7449c3b155c9ea3e55ae6
vinhvq545 <vinh.vq@isdcorp.vn>
2024-05-15 15:52:23 +0700

    update MQTT

---
+++ Server/IOT_SOCKET_BE/src/Services/IOT_SOCKET_BE.API/MQTT/MQTTService.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using Microsoft.Extensions.Hosting;
+
+namespace IOT_SOCKET_BE.MQTT
+{
+    public class MQTTService : IHostedService
+    {
+        private MqttClient _mqttClient;
+        private readonly string brokerAddress = "c805b8e62c2044939cc9ebcada8a35ee.s1.eu.hivemq.cloud";
+        private readonly int port = 8883;
+        private readonly string username = "vinhvq";
+        private readonly string password = "Voquangvinh2552001";
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                _mqttClient = new MqttClient(brokerAddress, port, true, MqttSslProtocols.TLSv1_2, null, null);
+                _mqttClient.MqttMsgPublishReceived += MqttClient_MqttMsgPublishReceived;
+
+                string clientId = Guid.NewGuid().ToString();
+                _mqttClient.Connect(clientId, username, password);
+
+                _mqttClient.Subscribe(new string[] { "socket/info" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+
+                Console.WriteLine("Connected to MQTT broker and subscribed to topic 'socket/info'");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error connecting to MQTT broker: {ex.Message}");
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private void MqttClient_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string message = Encoding.UTF8.GetString(e.Message);
+            Console.WriteLine($"Received message: {message}");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _mqttClient.Disconnect();
+            return Task.CompletedTask;
+        }
+    }
+}
--- Server/IOT_SOCKET_BE/src/Services/IOT_SOCKET_BE.API/Startup.cs
+++ Server/IOT_SOCKET_BE/src/Services/IOT_SOCKET_BE.API/Startup.cs
@@ -1,3 +1,4 @@
+using IOT_SOCKET_BE.MQTT;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,7 @@ namespace IOT_SOCKET_BE
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "IOT_SOCKET_BE", Version = "v1" });
             });
+            services.AddHostedService<MQTTService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
